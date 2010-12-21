2ae7cd82e1db7d7969262163c728f67998e0579c
Robert Spillner <Robert.Spillner@gmx.de>
2010-12-21 23:36:25 +0100

    use teacherDAO

--- src/main/java/org/mpg/grader/pages/teacher/CreateFromListTeacher.java
+++ src/main/java/org/mpg/grader/pages/teacher/CreateFromListTeacher.java
@@ -2,9 +2,8 @@ package org.mpg.grader.pages.teacher;
 
 import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.hibernate.annotations.CommitAfter;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.hibernate.Session;
+import org.mpg.grader.data.TeacherDAO;
 import org.mpg.grader.entities.Teacher;
 import org.mpg.grader.pages.Teachers;
 
@@ -14,19 +13,18 @@ public class CreateFromListTeacher {
 	private String teacherList;
 
 	@Inject
-	private Session session;
+	private TeacherDAO teacherDAO;
 
     @InjectPage
     private Teachers teachers;
 
-    @CommitAfter
     Teachers onSuccess()
     {
     	String[] teacherArray = teacherList.split(";");
     	for(String teacher : teacherArray) {
     		Teacher t = new Teacher();
     		t.setLastName(teacher.trim());
-    		session.persist(t);
+    		teacherDAO.add(t);
     	}
 
         return teachers;
